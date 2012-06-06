113508c762a24919a12b13feca0e68cbed1f77d8
jayandra <root@jayandra-Latitude-D520.(none)>
2012-06-06 13:50:52 +0545

    - initial devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Stripetest::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "my_devise/registrations" }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +50,7 @@ Stripetest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
